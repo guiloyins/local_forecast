e819870a43f61ec24a2ee18fc88eec214697a941
Rafael Rose <guiloyins@gmail.com>
2024-09-30 16:57:03 -0400

    Removd a deprecated config and active record reference

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
